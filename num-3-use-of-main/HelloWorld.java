085fe6a5391c3322b4dc5da54d5e0c1fafffa6de
EsinShadrach <esinshadrachrafe@gmail.com>
2025-05-09 21:34:34 +0100

    number-3

---
+++ num-3-use-of-main/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * The main method in Java is the entry point of any standalone Java application.
+ * It tells the Java Virtual Machine (JVM) where to begin execution of your program.
+ * */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
