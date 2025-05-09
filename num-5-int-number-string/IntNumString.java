05e448567a4caf33bffdd79008d2e7d6ba192cc9
EsinShadrach <esinshadrachrafe@gmail.com>
2025-05-09 21:40:09 +0100

    number -5

---
+++ num-5-int-number-string/IntNumString.java
@@ -0,0 +1,18 @@
+// What is the difference between int, Integer, and String?
+
+// Explanation:
+// int is a primitive data type that is used to store integer values.
+// Integer is a reference data type that is used to store integer values.
+// String is a reference data type that is used to store string values. such as words, sentences. E.t.c.
+public class IntNumString {
+    public static void main(String[] args) {
+        int i = 5;
+        Integer j = 5;
+        String s = "5";
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(s);
+    }
+}
+
